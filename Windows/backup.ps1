d97db2e6bc63c398f63dd085bade0de1fee23de0
Nanami <i@shiraha.cn>
2023-06-27 00:16:41 +0800

    Add Windows Terminal settings file.

--- Windows/backup.ps1
+++ Windows/backup.ps1
@@ -1,3 +1,20 @@
+### Items to copy
 $PowershellCoreProfileDir = Split-Path $PROFILE
-$Destination = Join-Path $PSScriptRoot "/Powershell"
-Copy-Item -Path $PowershellCoreProfileDir -Destination $Destination -Recurse
+$TerminalSettingsPath = Join-Path $env:LOCALAPPDATA "Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json"
+
+### Tools
+function New-DirectoryRecursively {
+    param(
+        [Parameter(Mandatory=$true)]
+        [string]$Path
+    )
+    if (-not (Test-Path -Path $Path -PathType Container)) {
+        New-Item -ItemType Directory -Path $Path -Force | Out-Null
+    }
+}
+
+
+### Run copy
+Copy-Item -Path $PowershellCoreProfileDir -Destination $PSScriptRoot -Recurse -Force
+New-DirectoryRecursively -Path (Join-Path $PSScriptRoot "/Terminal/")
+Copy-Item -Path $TerminalSettingsPath -Destination (Join-Path $PSScriptRoot "/Terminal/")
