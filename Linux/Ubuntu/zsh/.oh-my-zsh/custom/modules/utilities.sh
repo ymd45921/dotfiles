17e94f5c37ecf2575666ff3b53422d5944aa146b
nnm <i@shiraha.cn>
2023-11-13 17:29:46 +0800

    Add `tar_backup_system` with no-test.

--- Linux/Ubuntu/zsh/.oh-my-zsh/custom/modules/utilities.sh
+++ Linux/Ubuntu/zsh/.oh-my-zsh/custom/modules/utilities.sh
@@ -6,3 +6,10 @@ mount_onedrive() {
     fi
     rclone mount OneDrive:/ $OneDriveRoot --vfs-cache-mode full --copy-links --daemon
 }
+
+# Not run
+tar_backup_system() {
+    sudo tar -cvpjf "$1" --exclude="$1" --exclude=/lost+found --exclude=/proc --exclude=/run    \
+        --exclude=/dev --exclude=/tmp --exclude=/media --exclude="$OneDriveRoot"                \
+        /
+}
