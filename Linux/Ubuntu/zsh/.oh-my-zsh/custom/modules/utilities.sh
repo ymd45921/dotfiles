2516d1f994017cf98af3bbcc283106cd4cbc79da
nnm <i@shiraha.cn>
2023-11-13 17:44:20 +0800

    Add dirs to exclude in tar=system-backup

--- Linux/Ubuntu/zsh/.oh-my-zsh/custom/modules/utilities.sh
+++ Linux/Ubuntu/zsh/.oh-my-zsh/custom/modules/utilities.sh
@@ -1,4 +1,5 @@
 OneDriveRoot=/home/$(whoami)/OneDrive
+Home=/home/$(whoami)
 
 mount_onedrive() {
     if df -h | grep -q "$OneDriveRoot"; then
@@ -11,5 +12,9 @@ mount_onedrive() {
 tar_backup_system() {
     sudo tar -cvpjf "$1" --exclude="$1" --exclude=/lost+found --exclude=/proc --exclude=/run    \
         --exclude=/dev --exclude=/tmp --exclude=/media --exclude="$OneDriveRoot"                \
+        --exclude="$Home/.cache" --exclude="$Home/.ccache" --exclude="$Home/.gvfs"              \
+        --exclude="$Home/.Private" --exclude="$Home/.var/app/*/cache" --exclude=/sys            \
+        --exclude=/var/cache/* --exclude=/mnt --exclude=/var/log --exclude=/swapfile            \
+        --exclude="/snap/*/*/.cache" --exclude="$Home/.local/share/Trash"                       \
         /
 }
