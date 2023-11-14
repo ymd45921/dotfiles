a399fe37b60e68ca2dbb9f53b74b34f6ff8bb55c
nnm <i@shiraha.cn>
2023-11-15 02:30:04 +0800

    Add 2 functions for font installing.

--- Windows/Powershell/Modules/user-custom/MPG-H410/Utilities.ps1
+++ Windows/Powershell/Modules/user-custom/MPG-H410/Utilities.ps1
@@ -98,3 +98,113 @@ function Add-WindowsDefenderExclusionRule {
     }
     Set-MpPreference -ExclusionPath $existingExclusions
 }
+
+### Install fonts for current user
+function Install-FontsForCurrentUser {
+    [CmdletBinding()]
+    Param (
+        [string[]]$Files,
+        [string]$File
+    )
+    $objShell = New-Object -ComObject Shell.Application
+    $Fonts = $objShell.NameSpace(20)
+    If (!($Files -eq $null)){  Get-ChildItem "$Files\*.ttf" | ForEach-Object {$Fonts.CopyHere($_.FullName)} }
+    ElseIf (!($File -eq $null)){ $Fonts.CopyHere($File) }
+}
+Set-Alias Install-Fonts Install-FontsForCurrentUser
+Set-Alias Add-Fonts Install-FontsForCurrentUser
+
+#######################################################################################################################
+#######################################################################################################################
+#######################################################################################################################
+
+### Install or remove fonts for all users
+# From Internet
+# 1. Copy font-file to C:\Windows\Fonts
+# 2. Add an entry in HKLM\Software\Microsoft\Windows NT\CurrentVersion\Fonts
+function Install-Font {  
+    param (  
+        [System.IO.FileInfo]$fontFile  
+    )  
+    try { 
+
+        #get font name
+        $gt = [Windows.Media.GlyphTypeface]::new($fontFile.FullName)
+        $family = $gt.Win32FamilyNames['en-us']
+        if ($null -eq $family) { $family = $gt.Win32FamilyNames.Values.Item(0) }
+        $face = $gt.Win32FaceNames['en-us']
+        if ($null -eq $face) { $face = $gt.Win32FaceNames.Values.Item(0) }
+        $fontName = ("$family $face").Trim() 
+            
+        switch ($fontFile.Extension) {  
+            ".ttf" {$fontName = "$fontName (TrueType)"}  
+            ".otf" {$fontName = "$fontName (OpenType)"}  
+        }  
+
+        write-host "Installing font: $fontFile with font name '$fontName'"
+
+        If (!(Test-Path ("$($env:windir)\Fonts\" + $fontFile.Name))) {  
+            write-host "Copying font: $fontFile"
+            Copy-Item -Path $fontFile.FullName -Destination ("$($env:windir)\Fonts\" + $fontFile.Name) -Force 
+        } else {  write-host "Font already exists: $fontFile" }
+
+        If (!(Get-ItemProperty -Name $fontName -Path "HKLM:\Software\Microsoft\Windows NT\CurrentVersion\Fonts" -ErrorAction SilentlyContinue)) {  
+            write-host "Registering font: $fontFile"
+            New-ItemProperty -Name $fontName -Path "HKLM:\Software\Microsoft\Windows NT\CurrentVersion\Fonts" -PropertyType string -Value $fontFile.Name -Force -ErrorAction SilentlyContinue | Out-Null  
+        } else {  write-host "Font already registered: $fontFile" }
+            
+        [System.Runtime.Interopservices.Marshal]::ReleaseComObject($oShell) | out-null 
+        Remove-Variable oShell               
+                
+    } catch {            
+        write-host "Error installing font: $fontFile. " $_.exception.message
+    }
+}
+function Uninstall-Font {  
+    param (  
+        [System.IO.FileInfo]$fontFile  
+    )       
+    try { 
+        #get font name
+        $gt = [Windows.Media.GlyphTypeface]::new($fontFile.FullName)
+        $family = $gt.Win32FamilyNames['en-us']
+        if ($null -eq $family) { $family = $gt.Win32FamilyNames.Values.Item(0) }
+        $face = $gt.Win32FaceNames['en-us']
+        if ($null -eq $face) { $face = $gt.Win32FaceNames.Values.Item(0) }
+        $fontName = ("$family $face").Trim()
+            
+        switch ($fontFile.Extension) {  
+            ".ttf" {$fontName = "$fontName (TrueType)"}  
+            ".otf" {$fontName = "$fontName (OpenType)"}  
+        }  
+
+        write-host "Uninstalling font: $fontFile with font name '$fontName'"
+
+        If (Test-Path ("$($env:windir)\Fonts\" + $fontFile.Name)) {  
+            write-host "Removing font: $fontFile"
+            Remove-Item -Path "$($env:windir)\Fonts\$($fontFile.Name)" -Force 
+        } else {  write-host "Font does not exist: $fontFile" }
+
+        If (Get-ItemProperty -Name $fontName -Path "HKLM:\Software\Microsoft\Windows NT\CurrentVersion\Fonts" -ErrorAction SilentlyContinue) {  
+            write-host "Unregistering font: $fontFile"
+            Remove-ItemProperty -Name $fontName -Path "HKLM:\Software\Microsoft\Windows NT\CurrentVersion\Fonts" -Force                      
+        } else {  write-host "Font not registered: $fontFile" }
+            
+        [System.Runtime.Interopservices.Marshal]::ReleaseComObject($oShell) | out-null 
+        Remove-Variable oShell               
+                
+    } catch {            
+        write-host "Error uninstalling font: $fontFile. " $_.exception.message
+    }        
+}
+function Install-AllFontsInDir {
+    param (
+        [Parameter(Mandatory = $true)]
+        [ValidateScript({Test-Path $_})]
+        [string]$FontsDir
+    )
+    foreach ($FontItem in (Get-ChildItem -Path $FontsDir | 
+        Where-Object {($_.Name -like '*.ttf') -or ($_.Name -like '*.otf') })) {  
+    Install-Font -fontFile $FontItem.FullName  
+}  
+}
