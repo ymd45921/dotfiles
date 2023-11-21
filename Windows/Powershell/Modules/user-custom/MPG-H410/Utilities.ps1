f4f1572e24d046d89e352f629cd11a7db02c1126
nnm <i@shiraha.cn>
2023-11-21 23:52:35 +0800

    Add Find-AnimeSharing with Copilot(?)

--- Windows/Powershell/Modules/user-custom/MPG-H410/Utilities.ps1
+++ Windows/Powershell/Modules/user-custom/MPG-H410/Utilities.ps1
@@ -294,6 +294,17 @@ function Update-PowershellCore {
 Set-Alias pwsh-update Update-PowershellCore
 Set-Alias update-pwsh Update-PowershellCore
 
+### Search in Anime-sharing.com
+function Find-AnimeSharing {
+    param(
+        [Parameter(Mandatory = $true)]
+        [string]$Search
+    )
+    $part = [System.Uri]::EscapeDataString($Search)
+    Start-Process "https://www.anime-sharing.com/search/8619620/?q=$part&t=post&c[child_nodes]=1&c[nodes][0]=47&o=relevance"
+}
+Set-Alias assearch Find-AnimeSharing
+
 #######################################################################################################################
 #######################################################################################################################
 #######################################################################################################################
