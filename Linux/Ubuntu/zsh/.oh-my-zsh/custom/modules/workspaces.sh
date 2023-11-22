e0171a1a0a6e27a5f6a678a6e11e7d4b0b676664
nnm <i@shiraha.cn>
2023-11-22 19:46:11 +0800

    Add workspace.sh

---
+++ Linux/Ubuntu/zsh/.oh-my-zsh/custom/modules/workspaces.sh
@@ -0,0 +1,7 @@
+OMD_DIR=~/Documents/my-dotfiles
+alias omd="code $OMD_DIR"
+alias acm="code ~/Documents/Electric-Circus"
+alias restore-omd="$OMD_DIR/Linux/Ubuntu/recover.sh"
+alias backup-omd="$OMD_DIR/Linux/Ubuntu/backup.sh"
+alias omd-restore="$OMD_DIR/Linux/Ubuntu/recover.sh"
+alias omd-backup="$OMD_DIR/Linux/Ubuntu/backup.sh"
