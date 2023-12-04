3946f27284d334f0a413d5b032edc8927d08834c
nnm <i@shiraha.cn>
2023-12-04 22:19:38 +0800

    Add Ubuntu desktop shortcut creator.

--- Linux/Ubuntu/zsh/.oh-my-zsh/custom/modules/utilities.sh
+++ Linux/Ubuntu/zsh/.oh-my-zsh/custom/modules/utilities.sh
@@ -8,7 +8,6 @@ mount_onedrive() {
     rclone mount OneDrive:/ $OneDriveRoot --vfs-cache-mode full --copy-links --daemon
 }
 
-# Not run
 tar_backup_system() {
     sudo tar -cvpjf "$1" --exclude="$1" --exclude=/lost+found --exclude=/proc --exclude=/run    \
         --exclude=/dev --exclude=/tmp --exclude=/media --exclude="$OneDriveRoot"                \
@@ -17,4 +16,37 @@ tar_backup_system() {
         --exclude=/var/cache --exclude=/mnt --exclude=/var/log --exclude=/swapfile              \
         --exclude="/snap/*/*/.cache" --exclude="$Home/.local/share/Trash"                       \
         /
+}
+
+create_desktop_shortcut() {
+    if [ "$#" -eq 0 ]; then
+        echo "Usage: create_desktop_shortcut <target_path> <shortcut_name>"
+        return 1
+    fi
+    if [ ! -e "$1" ]; then
+        echo "Error: Target file does not exist."
+        return 1
+    fi
+    resolved_path=$(realpath "$1")
+    executable_name=$(basename "$resolved_path")
+    desktop_path="$HOME/Desktop"
+    if [ ! -d "$desktop_path" ]; then
+        if [ ! -e "$HOME/桌面" ]; then
+            echo "Error: Desktop path does not exist."
+            return 1
+        else
+            ln -s "$HOME/桌面" "$desktop_path"
+        fi
+    fi
+    shortcut_path="$desktop_path/$2.desktop"
+
+    # ? any tool like appimaged?
+    echo "[Desktop Entry]
+    Type=Application
+    Name=$2
+    Exec=$resolved_path
+    Terminal=false
+    Icon=$resolved_path.png" > "$shortcut_path"
+
+    echo "Desktop shortcut created: $shortcut_path"
 }
