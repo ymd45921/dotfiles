440096b350c54085f303b3cb31021ca42e0cb829
nnm <i@shiraha.cn>
2023-12-17 14:27:30 +0800

    Add blog workspace for MacOS.

--- MacOS/zsh/.oh-my-zsh/custom/modules/workspaces.sh
+++ MacOS/zsh/.oh-my-zsh/custom/modules/workspaces.sh
@@ -1,1 +1,16 @@
+### Workspaces definitions
+MY_WORKSPACES_ROOT=~/Source
+OMD_DIR=~/Source/dotfiles
+ACM_DIR=~/Source/Electric-Circus
+BLOG_ROOT=~/Source/blog
+BLOG_DIR=$BLOG_ROOT/Hexo-ymd45921
+
+### Workspaces aliases
 alias mpg="ssh i@lsz.ddns.net"
+alias omd="code $OMD_DIR"
+alias acm="code $ACM_DIR"
+alias restore-omd="$OMD_DIR/MacOS/recover.sh"
+alias backup-omd="$OMD_DIR/MacOS/backup.sh"
+alias omd-restore="$OMD_DIR/MacOS/recover.sh"
+alias omd-backup="$OMD_DIR/MacOS/backup.sh"
+alias blog="code $BLOG_DIR"
