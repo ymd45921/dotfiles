ed8b7680b0843698eb010d400076a47af2de0725
nnm <i@shiraha.cn>
2024-09-29 00:34:55 +0800

    Add environment variavle to hide venv prompt.

--- Windows/Powershell/Microsoft.PowerShell_profile.MPG-H410.ps1
+++ Windows/Powershell/Microsoft.PowerShell_profile.MPG-H410.ps1
@@ -1,6 +1,7 @@
 ### Initialize Oh-My-Posh Ver 3
 $OhMyPosh3Theme = Join-Path $PwshProfileDir '\themes\chips-modifiled.omp.json'
 oh-my-posh init pwsh --config $OhMyPosh3Theme | Invoke-Expression
+$env:VIRTUAL_ENV_DISABLE_PROMPT = 1     # to disable python venv prompt before oh-my-posh
 ### Initialize Oh-My-Posh Ver 2
 # Import-Module posh-git
 # Import-Module oh-my-posh
